d08fd82fcf4ea974b165ac384d6cd3f61891d4b3
jailcomfranssa <jail_son_0@hotmail.com>
2023-03-28 12:32:35 -0300

    trabalhando com arrays

---
+++ 04-Arrays e Estruturas de Repetição/arrays.js
@@ -0,0 +1,17 @@
+const alunos = ['João', 'Vitor', 'Marina'];
+console.log(alunos);
+console.log(alunos[0]);
+
+// Add aluno.
+alunos.push('Jose');
+alunos[4] = 'Vinicius'
+
+//Remove o ultimo
+alunos.pop();
+
+//remove o primeiro
+alunos.shift();
+
+//tamanho do array
+
+alunos.length;
